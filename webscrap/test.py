2895d9dee2d2543befdb2d47773856938e9cd54a
jiper <542062320@qq.com>
2018-04-09 18:01:33 +0800

    4.9

--- webscrap/test.py
+++ webscrap/test.py
@@ -7,9 +7,7 @@ Created on Mon Apr  2 13:25:31 2018
 """
 
 
-
-
-str='6,961'
-str1=str.replace(',','')
-print(str)
-print (str1)
+import subprocess
+filename=r'D:\downloadTest\600066_20171202_2'
+filename1=r'D:\downloadTest\600066_20171202_2.html'
+subprocess.call("C:/Users/suxiaolin/Desktop/2/pdf2htmlEX.exe pdf/"+filename+".pdf  --dest-dir  pdf/"+filename1, shell=True)
